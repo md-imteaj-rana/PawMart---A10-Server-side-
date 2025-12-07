cb872135ba463b8d6dd8bd5dff81c495ef4e2039
MD. Imteaj Rana <imteaj.rana2@gmail.com>
2025-12-07 18:55:27 +0600

    1st

---
+++ index.js
@@ -0,0 +1,46 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const port = 3000;
+
+const app = express()
+app.use(cors());
+
+
+//Mongodb code 
+
+
+const uri = "mongodb+srv://Pawmart-A10:imtepawmartA10@pawmarta10.t0jzost.mongodb.net/PawMartA10?retryWrites=true&w=majority&appName=PawMartA10";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close(); (we have to remove this line otherwise it will close the database automatically)
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/',(req,res)=>{
+    res.send('hello devlopers')
+})
+
+app.listen(port, ()=>{
+    console.log(`Server is running on port: ${port}`);
+})
